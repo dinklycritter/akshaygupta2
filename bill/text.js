9ef79d4ef86031cc07bd395511e4a91dba4365ed
dinklycritter <158404944+dinklycritter@users.noreply.github.com>
2025-05-13 10:24:32 -0700

    Update text.js

--- bill/text.js
+++ bill/text.js
@@ -1,6 +1,8 @@
 //text.js
-const good = ["bill posted a fire meme to mr clean.", "bill used logical fallacies to gain more followers.", "bill has immense aura."]
-const bad = ["bill fumbled hard and got little followers.", "bill used logical fallacies, but someone pointed it out.", "bill posted unfunny memes to mr clean."]
-const mid = ["bill has been declared mid by the masses.", "bill is neither good nor bad.", "bill is mid."]
-const zero = ["NOBODY CARES ABOUT BILL!!", "bill did not gain any supporters.", "people don't like bill."]
-const negative = ["i shit my pants.", "what did bill do bro he lost followers.", "THE PEOPLE HATE BILL!!!!"]
+const good = ["bill posted a fire meme to mr clean.", "bill used logical fallacies to gain more followers.", "bill has immense aura."];
+const bad = ["bill fumbled hard and got little followers.", "bill used logical fallacies, but someone pointed it out.", "bill posted unfunny memes to mr clean."];
+const mid = ["bill has been declared mid by the masses.", "bill is neither good nor bad.", "bill is mid."];
+const zero = ["NOBODY CARES ABOUT BILL!!", "bill did not gain any supporters.", "people don't like bill."];
+const negative = ["i shit my pants.", "what did bill do bro he lost followers.", "THE PEOPLE HATE BILL!!!!"];
+
+module.exports = {good, bad, mid, zero, negative};
